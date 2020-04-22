d8ddd5e7e27bd80ebf0a0837c775f494b6755f1c
tbircek-beckwith <tbircek@beckwithelectric.com>
2020-04-22 11:59:13 -0400

    split project to multiple files

---
+++ OCCControl/Helpers/Declarations.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Threading
+Imports System.Text
+
+Imports RVBSim.Communication.Operations
+Imports RVBSim.PeriodicOperations
+
+Module Declarations
+
+    Friend Const ConsoleWriteEnable As Boolean = True       'False          ' 
+    Friend Const SupportedRVBRevision As String = "15"         'Supported RVB feature document revision
+    Friend Const OperatingVoltage As Integer = 900
+    Friend Const M2001D_Comm_Scale As Integer = 10
+    Friend Const MaxDeltaVoltage As Integer = 100
+    Friend Const MinDeltaVoltage As Integer = -100
+    Friend Const DeltaMessage As String = "Local Voltage + "
+    Friend Const DirectMessage As String = "RVB Voltage is ="
+    Friend Const DNP_BufferSize As Integer = 29
+    Friend Const Modbus_BufferSize As Integer = 12
+    Friend Const IEC_BufferSize As Integer = 200
+
+    Friend sb As New StringBuilder
+    Friend IPs As String() = New String(1) {}
+
+    Friend start As New StartOperation
+    Friend pause As New PauseOperation
+
+    Friend periodicRead As New ReadEvents
+    Friend periodicWrite As New WriteEvents
+    Friend periodicReset As New ResetEvents
+
+    Friend dnp As tcpdnp.AsyncDNP3_0
+    Friend modbus As tcpmodbus.AsyncModbus
+    Friend iec61850 As iec.AsyncIEC61850
+
+    Friend processID As Integer = 0
+    Friend support As Boolean      'True rev 15 or greater False rev 8
+    Friend errorCounter As Integer = 0
+
+    Friend WriteTickerDone As New ManualResetEvent(False)
+    Friend ReadTickerDone As New ManualResetEvent(False)
+    Friend TimersEvent As New ManualResetEvent(False)
+
+    Friend WriteRegisterWait As RegisteredWaitHandle
+    Friend ReadRegisterWait As RegisteredWaitHandle
+
+    Friend visibility As Boolean = True
+    Friend testSetting As TestSettings
+    Friend modbusRegister As ModbusSettings
+    Friend dnpSetting As DnpSettings
+    Friend iecSetting As IECSettings
+
+    Friend Structure TestSettings
+        Dim Protocol As String
+        Dim readIpAddress As String
+        Dim writeIpAddress As String
+        Dim HeartbeatTimer As UShort
+        Dim FwdRVBVoltage As Double
+        Dim RevRVBVoltage As Double
+        Dim FwdRVBVoltageScale As Double
+        Dim RevRVBVoltageScale As Double
+        Dim RVBMax As Double
+        Dim RVBMin As Double
+    End Structure
+
+    Friend Structure ModbusSettings
+        Dim Port As String
+        Dim LocalVoltage As UShort
+        Dim RVBEnable As UShort
+        Dim FRVBValue As UShort
+        Dim FRVBScale As UShort
+        Dim RVBHeartBeatTimer As UShort
+        Dim RVBActive As UShort
+        Dim RRVBValue As UShort
+        Dim RRVBScale As UShort
+        Dim RVBMax As UShort
+        Dim RVBMin As UShort
+        Dim Factory As UShort
+    End Structure
+
+    Friend Structure DnpSettings
+        Dim Port As String
+        Dim source As UShort
+        Dim destination As UShort
+        'just get value of point, DNP only cares about the point values
+        Dim LocalVoltage As UShort
+        Dim RVBEnable As UShort
+        Dim FRVBValue As UShort
+        Dim FRVBScale As UShort
+        Dim RVBHeartBeatTimer As UShort
+        Dim RRVBValue As UShort
+        Dim RRVBScale As UShort
+        Dim RVBMax As UShort
+        Dim RVBMin As UShort
+    End Structure
+
+    Friend Structure IECSettings
+        Dim Port As String
+        Dim ReadIEDName As String
+        Dim WriteIEDName As String
+        Dim iedClass As String
+        Dim LocalVoltage As String
+        Dim RVBEnable As String
+        Dim FRVBValue As String
+        Dim FRVBScale As String
+        Dim RVBHeartBeatTimer As String
+        Dim RRVBValue As String
+        Dim RRVBScale As String
+        Dim RVBMax As String
+        Dim RVBMin As String
+    End Structure
+
+    Private _WriteInterval As Integer
+    Friend Property WriteInterval() As Integer
+        Get
+            Return _WriteInterval
+        End Get
+        Set(ByVal value As Integer)
+            _WriteInterval = value
+        End Set
+    End Property
+
+    Private _ReadInterval As Integer
+    Friend Property ReadInterval() As Integer
+        Get
+            Return _ReadInterval
+        End Get
+        Set(ByVal value As Integer)
+            _ReadInterval = value
+        End Set
+    End Property
+
+    Private _protocolinuse As String
+    Friend Property ProtocolInUse() As String
+        Get
+            Return _protocolinuse
+        End Get
+        Set(ByVal value As String)
+            _protocolinuse = value
+        End Set
+    End Property
+
+    Private _Heart_Beat_Timer As Double = 0.0
+    Friend Property Heart_Beat_Timer() As Double
+        Get
+            Return _Heart_Beat_Timer
+        End Get
+        Set(ByVal value As Double)
+            _Heart_Beat_Timer = value
+        End Set
+    End Property
+
+    Private _ActualLocalVoltage As Double = 0.0
+    Friend Property ActualLocalVoltage() As Double
+        Get
+            Return _ActualLocalVoltage
+        End Get
+        Set(ByVal value As Double)
+            _ActualLocalVoltage = value
+        End Set
+    End Property
+
+    Private _Forward_RVBVoltage2Write As Double = 0.0
+    Friend Property Forward_RVBVoltage2Write() As Double
+        Get
+            Return _Forward_RVBVoltage2Write
+        End Get
+        Set(ByVal value As Double)
+            _Forward_RVBVoltage2Write = value
+        End Set
+    End Property
+
+    Private _Reverse_RVBVoltage2Write As Double = 0.0
+    Friend Property Reverse_RVBVoltage2Write() As Double
+        Get
+            Return _Reverse_RVBVoltage2Write
+        End Get
+        Set(ByVal value As Double)
+            _Reverse_RVBVoltage2Write = value
+        End Set
+    End Property
+
+    Private _readresult As UShort = 0
+    Friend Property readresult() As UShort
+        Get
+            Return _readresult
+        End Get
+        Set(ByVal value As UShort)
+            _readresult = value
+        End Set
+    End Property
+
+    Private _errorMsg As String = ""
+    Friend Property ReceivedErrorMsg() As String
+        Get
+            Return _errorMsg
+        End Get
+        Set(ByVal value As String)
+            _errorMsg = value
+        End Set
+    End Property
+
+End Module
---
+++ OCCControl/Helpers/PopulateTheForm.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Threading
+
+Module Populate
+
+    Friend Sub Populatetheform()
+        Try
+            Dim xmlRead As New ReadXmlFile
+            xmlRead.read()
+            RVBSim.Text = String.Format("RVB Simulator v-{0}.{1}", My.Application.Info.Version.Major, My.Application.Info.Version.Minor)
+
+            If CInt(SupportedRVBRevision) >= 15 Then
+                support = True
+                RVBSim.grpRevSettings.Visible = True
+            Else
+                support = False
+                RVBSim.grpRevSettings.Visible = False
+            End If
+
+            ' With My.Settings
+            RVBSim.NumericUpDownDNPSourceAddress.Value = dnpSetting.source              '.source
+            RVBSim.NumericUpDownDNPDestinationAddress.Value = dnpSetting.destination   '.destination
+
+            RVBSim.NumericUpDownModbusLocalVoltageRegister.Value = modbusRegister.LocalVoltage  '.mdLocalvoltage
+            RVBSim.NumericUpDownModbusFwdRVBVoltageRegister.Value = modbusRegister.FRVBValue  '.mdFRVBvoltage
+            RVBSim.NumericUpDownModbusRevRVBVoltageRegister.Value = modbusRegister.RRVBValue  '.mdRRVBvoltage
+
+            RVBSim.heartbeattimer.Value = testSetting.HeartbeatTimer                  '.heartbeat
+            If testSetting.FwdRVBVoltage < MinDeltaVoltage Or testSetting.FwdRVBVoltage > MaxDeltaVoltage Then RVBSim.FwdDeltaVoltage.Value = 0.0 Else RVBSim.FwdDeltaVoltage.Value = testSetting.FwdRVBVoltage
+            If testSetting.RevRVBVoltage < MinDeltaVoltage Or testSetting.RevRVBVoltage > MaxDeltaVoltage Then RVBSim.RevDeltaVoltage.Value = 0.0 Else RVBSim.RevDeltaVoltage.Value = testSetting.RevRVBVoltage
+
+            RVBSim.FwdRVBScaleFactor.Value = testSetting.FwdRVBVoltageScale  '.Fmultiplier
+            RVBSim.RevRVBScaleFactor.Value = testSetting.RevRVBVoltageScale   '.Rmultiplier
+            RVBSim.txtIECLocalVoltage.Text = iecSetting.LocalVoltage       '.IECLocalVoltage
+            RVBSim.txtIECFwdRVBVoltage.Text = iecSetting.FRVBValue         ' .IECFwdRVBVoltage
+            RVBSim.txtIECRevRVBVoltage.Text = iecSetting.RRVBValue         ' .IECRevRVBVoltage
+
+            With My.Settings
+                '************************************************************
+                '*  if visibility set to true by the user                   *
+                '*  Verify the form is in visible area                      *
+                '*  If NOT reset location to 0,0                            *
+                '************************************************************
+                If visibility Then
+                    Dim x As Integer = My.Computer.Screen.WorkingArea.Left
+                    Dim y As Integer = My.Computer.Screen.WorkingArea.Right
+                    If .location.X < x Or .location.X > y Then
+                        RVBSim.DesktopLocation = New System.Drawing.Point(20, 20)
+                    Else
+                        RVBSim.DesktopLocation = .location
+                    End If
+                Else
+                    RVBSim.DesktopLocation = New System.Drawing.Point(32000, 32000)
+                    ' Me.DesktopLocation = New System.Drawing.Point(20, 20)
+                End If
+                '************************************************************
+            End With
+            Select Case testSetting.Protocol    '.protocol
+                Case "dnp"
+                    RVBSim.dnpbutton.Checked = True
+                    RVBSim.txtPort.Text = dnpSetting.Port  '.dnpport
+                    RVBSim.checkHandler(RVBSim.dnpbutton)
+                Case "modbus"
+                    RVBSim.modbusbox.Checked = True
+                    RVBSim.txtPort.Text = modbusRegister.Port  '.mdport
+                    RVBSim.checkHandler(RVBSim.modbusbox)
+                Case "iec"
+                    RVBSim.iec61850box.Checked = True
+                    RVBSim.txtPort.Text = iecSetting.Port '.iecport
+                    RVBSim.checkHandler(RVBSim.iec61850box)
+            End Select
+            RVBSim.txtRead.Text = testSetting.readIpAddress  '.IPAddressToRead
+            RVBSim.txtWrite.Text = testSetting.writeIpAddress
+            '
+        Catch ex As Exception
+            SetText(RVBSim.lblMsgCenter, ex.Message)
+            sb.AppendLine(String.Format("{0} {1}", Now, ex.Message))
+        Finally
+            If ConsoleWriteEnable Then Console.WriteLine("Current thread is # {0} --- populatetheform", Thread.CurrentThread.GetHashCode)
+        End Try
+    End Sub
+
+End Module
