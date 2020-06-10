65e6d313173bbf95bdb7bb21a58abbc75dba427a
tbircek <tbircek@beckwithelectric.com>
2020-06-10 06:51:03 -0400

    adds IEnumerable

--- rvb-sim/Operations/Communication/Regulator.vb
+++ rvb-sim/Operations/Communication/Regulator.vb
@@ -2,7 +2,10 @@
 ''' <summary>
 ''' Holds all information about the regulator(s)
 ''' </summary>
-Public Class Regulator
+Public Class Regulator '(Of CommunicationModels)
+    ' Implements System.Collections.IEnumerable
+
+    'Public Property CommunicationModels() As CommunicationBaseModel(Of T)
 
     ''' <summary>
     ''' Collection of dnp3.0 communication details
@@ -22,5 +25,53 @@ Public Class Regulator
     ''' <returns></returns>
     Public Property IECCommunication As ObservableCollection(Of IECCommunicationModel) = New ObservableCollection(Of IECCommunicationModel)
 
+    'Public Function GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+    '    ' Throw New NotImplementedException()
+    '    ' Dim combine As IEnumerator
+    '    'Return New RegulatorEnumerator()
+
+    '    Dim output As List(Of Object) = New List(Of Object)
+
+    '    output.AddRange(DnpCommunication)
+    '    output.AddRange(ModbusCommunication)
+    '    output.AddRange(IECCommunication)
+
+    '    Return output
+    'End Function
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    Private Class RegulatorEnumerator
+        Implements IEnumerator
+
+        ' Private _regulator As List(Of T)
+        Private _dnpCommunication As ObservableCollection(Of DnpCommunicationModel)
+        Private _modbusCommunication() As ObservableCollection(Of ModbusCommunicationModel)
+        Private _iecCommunication As ObservableCollection(Of IECCommunicationModel)
+        Private _position As Integer = -1
+
+        Public Sub New(ByVal modbusCommunication() As ObservableCollection(Of ModbusCommunicationModel), ByVal iecCommunication As ObservableCollection(Of IECCommunicationModel), ByVal dnpCommunication As ObservableCollection(Of DnpCommunicationModel))
+            _modbusCommunication = modbusCommunication
+            _iecCommunication = iecCommunication
+            _dnpCommunication = dnpCommunication
+        End Sub
+
+        Public ReadOnly Property Current As Object Implements IEnumerator.Current
+            Get
+                Throw New NotImplementedException()
+                'Return
+            End Get
+        End Property
+
+        Public Sub Reset() Implements IEnumerator.Reset
+            Throw New NotImplementedException()
+        End Sub
+
+        Public Function MoveNext() As Boolean Implements IEnumerator.MoveNext
+            Throw New NotImplementedException()
+        End Function
+    End Class
 
 End Class
+
