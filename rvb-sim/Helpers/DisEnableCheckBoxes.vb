3d4b30dfc3d94ac54319141227870630fe4ca0b1
tbircek <tbircek@beckwithelectric.com>
2020-06-10 07:30:50 -0400

    code clean up

--- rvb-sim/Helpers/DisEnableCheckBoxes.vb
+++ rvb-sim/Helpers/DisEnableCheckBoxes.vb
@@ -2,47 +2,29 @@
 
 Module DisEnableCheckBoxes
 
+    ''' <summary>
     ''' Enables or disables controls per Start button
+    ''' </summary>
     Friend Sub Disenable()
 
         Try
             With RVBSim.StartButton
                 Debug.WriteLine($"Current thread is # {Thread.CurrentThread.GetHashCode} --- {NameOf(Disenable)}")
 
+                ' Set values and dis/enable of controls in Regulator List
                 SetValues(.Enabled)
 
-                ''dnp settings dis/enable
-                'SetEnable(RVBSim.DNPSourceReg1, .Enabled)
-                'SetEnable(RVBSim.DNPDestinationReg1, .Enabled)
-
-                ''modbus settings dis/enable
-                'SetEnable(RVBSim.ModbusLocalVoltageReg1, .Enabled)
-                'SetEnable(RVBSim.ModbusFRVBValueReg1, .Enabled)
-                'SetEnable(RVBSim.ModbusRRVBValueReg1, .Enabled)
-
-                ''iec61850 settings dis/enable
-                'SetEnable(RVBSim.IecLocalVoltageReg1, .Enabled)
-                'SetEnable(RVBSim.IecFRVBValueReg1, .Enabled)
-                'SetEnable(RVBSim.IecRRVBValueReg1, .Enabled)
+                ' Set Protocol options
+                SetEnable(RVBSim.ProtocolBox, .Enabled)
 
                 'communication settings dis/enable
+                ' except Start and Stop buttons
                 SetEnable(RVBSim.WriteIpAddr, .Enabled)
                 SetEnable(RVBSim.ReadIpAddr, .Enabled)
                 SetEnable(RVBSim.PortReg1, .Enabled)
 
-                'protocol options dis/enable
-                SetEnable(RVBSim.dnpbutton, .Enabled)
-                SetEnable(RVBSim.modbusbox, .Enabled)
-                SetEnable(RVBSim.iec61850box, .Enabled)
-
                 'general rvb settings dis/enable
-                SetEnable(RVBSim.heartBeatTimer, .Enabled)
-                SetEnable(RVBSim.useDeltaVoltage, .Enabled)
-                SetEnable(RVBSim.useFixedVoltage, .Enabled)
-                SetEnable(RVBSim.FRVBScaleReg1, .Enabled)
-                SetEnable(RVBSim.RVBMax, .Enabled)
-                SetEnable(RVBSim.RVBMin, .Enabled)
-                SetEnable(RVBSim.RevRVBScaleFactorReg1, .Enabled)
+                SetEnable(RVBSim.RVBSettings, .Enabled)
 
             End With
 
