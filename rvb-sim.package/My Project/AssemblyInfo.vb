6a009fcaae92cf845f087d6ac1bf3478d485b1dc
tbircek <tbircek@beckwithelectric.com>
2020-06-12 11:09:11 -0400

    renames projects

---
+++ rvb-sim.package/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Remote Voltage Bias Simulator")>
+<Assembly: AssemblyDescription("Reads from local voltage, modifies read local voltage, writes back to the remote voltage register")>
+<Assembly: AssemblyCompany("Beckwith Electric Co. Inc.")>
+<Assembly: AssemblyProduct("rvbsim.package")>
+<Assembly: AssemblyCopyright("Copyright © 2013-2020 Turgay Bircek")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bb708d16-03db-4de0-a89e-d247e86711a0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
