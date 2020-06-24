7d5f403e8e56c6aa32047f641e75a10c1b484363
tbircek <tbircek@beckwithelectric.com>
2020-06-24 15:16:06 -0400

    adds a new .json setting file

---
+++ rvbsim/Operations/Settings/BaseModel/JsonRegulatorModel.vb
@@ -0,0 +1,14 @@
+﻿Imports Newtonsoft.Json
+
+Public Class JsonRegulatorModel
+
+    ' JsonProperty names need to match the .json file and are case insensitive
+
+    <JsonProperty("Id")>
+    Public Property Id() As Integer
+
+    <JsonProperty("Value")>
+    Public Property Value() As Integer
+
+
+End Class
---
+++ rvbsim/Operations/Settings/ProtocolModels/Protocols/DnpProtocolSettingsModel.vb
@@ -0,0 +1,12 @@
+﻿Imports Newtonsoft.Json
+
+Public Class DnpProtocolSettingsModel
+    Inherits SettingsProtocolBaseModel
+
+    <JsonProperty("Source")>
+    Public Property Source() As Integer
+
+    <JsonProperty("Destination")>
+    Public Property Destination() As Integer
+
+End Class
