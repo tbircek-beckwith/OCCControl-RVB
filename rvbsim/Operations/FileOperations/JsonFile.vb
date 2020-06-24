10817d09bd0423c22ee2a0334e10c56c094f3807
tbircek <tbircek@beckwithelectric.com>
2020-06-24 15:18:12 -0400

    adds 3ph .json settings files

---
+++ rvbsim/Operations/FileOperations/JsonFile.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+Imports Newtonsoft.Json
+
+Public Class JsonFile
+
+#Region "Public Methods"
+
+    Public Sub New()
+
+        Dim baseFileLocation As String = Path.Combine(path1:=My.Application.Info.DirectoryPath,
+                                                          path2:="resources",
+                                                          path3:="Settings.json")
+
+        Dim dnpFileLocation As String = Path.Combine(path1:=My.Application.Info.DirectoryPath,
+                                                      path2:="resources",
+                                                          path3:="Settings-dnp.json")
+
+        Dim iecFileLocation As String = Path.Combine(path1:=My.Application.Info.DirectoryPath,
+                                                      path2:="resources",
+                                                          path3:="Settings-iec.json")
+
+        Dim modbusFileLocation As String = Path.Combine(path1:=My.Application.Info.DirectoryPath,
+                                                      path2:="resources",
+                                                          path3:="Settings-modbus.json")
+
+        baseJsonSettings = GetSettings(Of JsonSettingsModel)(baseFileLocation)
+        Dim dnpJson = GetSettings(Of DnpProtocolSettingsModel)(dnpFileLocation)
+        Dim iecJson = GetSettings(Of IecProtocolSettingsModel)(iecFileLocation)
+        Dim modbusJson = GetSettings(Of ModbusProtocolSettingsModel)(modbusFileLocation)
+
+        Debug.WriteLine("something")
+
+    End Sub
+#End Region
+
+#Region "Private Methods"
+
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <typeparam name="T"></typeparam>
+    ''' <param name="fileLocation"></param>
+    ''' <returns></returns>
+    Private Function GetSettings(Of T)(fileLocation As String) As T
+
+        Dim fileJson As T
+        Using reader = New StreamReader(fileLocation)
+
+            fileJson = JsonConvert.DeserializeObject(Of T)(reader.ReadToEnd())
+        End Using
+
+        Return fileJson
+    End Function
+#End Region
+
+End Class
