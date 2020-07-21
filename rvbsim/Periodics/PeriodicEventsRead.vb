45313046b7d11c74bdb9ab94270afed55c71cac2
tbircek <tbircek@beckwithelectric.com>
2020-07-21 10:37:21 -0400

    removes obsolete methods

--- rvbsim/Periodics/PeriodicEventsRead.vb
+++ rvbsim/Periodics/PeriodicEventsRead.vb
@@ -1,5 +1,4 @@
 ﻿Imports System.Threading
-'Imports tcpdnp
 Imports tcpdnp.AsyncDNP3_0
 
 Namespace PeriodicOperations
@@ -88,7 +87,11 @@ Namespace PeriodicOperations
 
                                     'For i = 0 To WriteRegisterWaits.Count - 1
 
-                                    SetText(rvbForm.lblLocalVoltageValue, $"Regulator: {regulator.Id}, Readings: Fwd Voltage: {FormatNumber(CDbl(LocalVoltageReadings(regulator.Id - 1) / BecoCommunicationScaleFactor), 1)}V {vbTab} Src Voltage: {FormatNumber(CDbl(SourceVoltageReadings(regulator.Id - 1) / BecoCommunicationScaleFactor), 1)}V")
+                                    '  SetText(rvbForm.lblLocalVoltageValue, $"Regulator: {regulator.Id}, Readings: Fwd Voltage: {FormatNumber(CDbl(LocalVoltageReadings(regulator.Id - 1) / BecoCommunicationScaleFactor), 1)}V {vbTab} Src Voltage: {FormatNumber(CDbl(SourceVoltageReadings(regulator.Id - 1) / BecoCommunicationScaleFactor), 1)}V")
+
+                                    ' SetText(rvbForm.SourceVoltageReadingRegulator1, $"Reading: {FormatNumber(CDbl(LocalVoltageReadings(regulator.Id - 1) / BecoCommunicationScaleFactor), 1)}V")
+
+                                    Dim s = New UpdateMeteringValues(rvbForm:=rvbForm, registerBox:=registerBox, regulatorId:=regulator.Id - 1)
 
                                     SetText(rvbForm.lblMsgCenter, $"Error: {ReceivedErrorMsg}")
 
@@ -142,175 +145,5 @@ Namespace PeriodicOperations
             End Try
         End Sub
 
-        ''' <summary>
-        ''' Reads
-        ''' </summary>
-        ''' <param name="rvbForm"></param>
-        <Obsolete("will remove next iteration", True)>
-        Protected Friend Sub Read(ByRef rvbForm As RVBSim)
-            Try
-
-                '' TODO: delete up to exit sub
-                'Debug.WriteLine($"{Date.Now:hh:mm:ss.ffff} -- {NameOf(Read)} is running... Reads everything.")
-
-                'Debug.WriteLine($"-----------------------> Elapsed time: {ReadingTimer.ElapsedMilliseconds} msec")
-                'Exit Sub
-
-                '' TODO: add loop to thru multi phase regulators
-
-                Debug.WriteLine($"Current thread is # {Thread.CurrentThread.GetHashCode} --- {NameOf(Read)} --- START")
-
-                Dim ReadEvent As New ManualResetEvent(False)
-
-                'LocalVoltageReadings = Enumerable.Repeat(Of UShort)(65535, 3).ToList()   ' New List(Of UShort) '(3)
-                'SourceVoltageReadings = Enumerable.Repeat(Of UShort)(65535, 3).ToList()  'New List(Of UShort) '(3)
-
-                For Each regulator In testJsonSettingsRegulators.Regulator
-
-
-                    Debug.WriteLine($"3-ph Active: {rvbForm.RVBSettings3Phase.Visible}")
-
-                    ' only send to regulator 1 when Single Phase checked
-                    If rvbForm.RVBSettings3Phase.Visible Or regulator.Id = 1 Then
-
-                        ' generate control names with regulator values.
-                        For Each value In regulator.Values
-
-                            Debug.Write($"value: {value.Name}")
-
-                            ' it is not a metering control
-                            If value.Name.Contains("RVB") Then
-                                Debug.WriteLine($" <------------------- SKIPPED ...")
-                                Continue For
-                            End If
-
-                            Dim settingControlName As String = $"{ProtocolInUse}{value.Name}Reg{regulator.Id}"
-                            Debug.WriteLine($" <------------------- {settingControlName} processing ...")
-
-                            Dim v() As Control = rvbForm.Controls.Find(settingControlName, True)
-
-                            If v.Length > 0 Then
-
-                                If v(0).Visible Then
-
-                                    Debug.Write($"--- {v(0).Name} is VISIBLE --- ")
-
-                                    Dim registerBox As NumericUpDown = CType(v(0), NumericUpDown)
-
-                                    Debug.WriteLine($"Control name: {registerBox.Name}, register: {registerBox.Value}, value: read this")
-                                    Debug.WriteLine($"------------------- Reading Local & Source Voltage ({ProtocolInUse.ToUpperInvariant()}) -------------------")
-
-                                    Select Case ProtocolInUse
-                                        Case "modbus"
-
-                                            'read the user specified single modbus register.
-                                            If registerBox.Name.Contains("Source") Then
-
-                                                Interlocked.Exchange(SourceVoltageReadings.Item(regulator.Id - 1), modbusRead.ReadHoldingRegisters(registerBox.Value, 1).ElementAt(0))
-                                            Else
-
-                                                Interlocked.Exchange(LocalVoltageReadings.Item(regulator.Id - 1), modbusRead.ReadHoldingRegisters(registerBox.Value, 1).ElementAt(0))
-                                            End If
-
-                                        Case "dnp"
-
-                                            ' updates "result" variable internally
-                                            dnp.Send(ManualEvent:=ReadEvent,
-                                                     Destination:=rvbForm.DNPDestinationReg1.Value,
-                                                     Source:=rvbForm.DNPSourceReg1.Value,
-                                                     FunctionCode:=Mode.Read,
-                                                     ObjectX:=Objects.AnalogInput,  ' .AnalogOutputStatus, '
-                                                     Variation:=Variations.AnaInput16bitVar4,   '.AnaOutBlockShort, '
-                                                     Qualifier:=QualifierField.AnaInput16bitStartStop,
-                                                     Start16Bit:=registerBox.Value,
-                                                     Stop16Bit:=registerBox.Value)
-
-                                            'read the user specified single dnp point.
-                                            If registerBox.Name.Contains("Source") Then
-
-                                                Interlocked.Exchange(SourceVoltageReadings.Item(regulator.Id - 1), result)
-                                            Else
-
-                                                Interlocked.Exchange(LocalVoltageReadings.Item(regulator.Id - 1), result)
-                                            End If
-
-                                            ReadEvent.WaitOne()
-                                            ReceivedErrorMsg = ErrorReceived
-
-                                        Case "iec"
-
-
-                                        Case Else
-
-                                    End Select
-
-                                    For i = 0 To SupportedRegulatorNumber - 1
-
-                                        SetText(rvbForm.lblLocalVoltageValue, $"Readings: Fwd Voltage: {FormatNumber(CDbl(LocalVoltageReadings(i) / BecoCommunicationScaleFactor), 1)}V {vbTab} Src Voltage: {FormatNumber(CDbl(SourceVoltageReadings(i) / BecoCommunicationScaleFactor), 1)}V")
-
-                                        SetText(rvbForm.lblMsgCenter, $"Error: {ReceivedErrorMsg}")
-
-                                        Debug.WriteLine($"Reading local voltage: {LocalVoltageReadings(i)} - source voltage: {SourceVoltageReadings(i)} - {HeartBeatTimers(i)}")
-                                    Next
-
-                                    'Debug.WriteLine($"Reading local voltageS: {LocalVoltageReadings(regulator.Id - 1)} - source voltage: { SourceVoltageReadings(regulator.Id - 1)} - {Heart_Beat_Timer}")
-                                    Debug.WriteLine($"------------------- Reading Local & Source Voltage ({ProtocolInUse.ToUpperInvariant()}) Done -------------------")
-
-                                End If
-                            End If
-
-                        Next
-
-                        Debug.WriteLine($"Reading local voltageS: {LocalVoltageReadings(regulator.Id - 1)} - source voltage: { SourceVoltageReadings(regulator.Id - 1)} - {HeartBeatTimers(regulator.Id - 1)}")
-                    End If
-                Next
-
-                For i = 0 To SupportedRegulatorNumber - 1
-
-                    Interlocked.Add(HeartBeatTimers(i), ReadInterval)
-
-                Next
-
-                ' Interlocked.Add(Heart_Beat_Timer, ReadInterval)
-                Debug.WriteLine($"Current thread Is # {Thread.CurrentThread.GetHashCode} {NameOf(Read)} --- END")
-
-                If Not ReceivedErrorMsg = "None" Then sb.AppendLine($"{Now} Received {ReceivedErrorMsg} error")
-
-                ReadEvent.SafeWaitHandle.Close()
-
-                '    ElseIf ProtocolInUse() = "iec" Then
-
-                '        For Each model As IECCommunicationModel In regulator.IECCommunication
-
-                '            Debug.WriteLine("------------------- Reading Local &  Source Voltage (61850) -------------------")
-
-                '            'read the user specified 61850 objects.
-                '            Dim localVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.LocalVoltage)}Reg1"))(0)    '{model.Id}"))
-
-                '            iec61850.Send(ReadEvent, localVoltage.Text, "Read")
-                '            ReadEvent.WaitOne()
-                '            LocalVoltageReadresult = iec.AsyncIEC61850.result
-                '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
-
-                '            'read the user specified 61850 objects.
-                '            Dim sourceVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.SourceVoltage)}Reg1"))(0)     '{model.Id}"))
-
-                '            iec61850.Send(ReadEvent, sourceVoltage.Text, "Read")
-                '            ReadEvent.WaitOne()
-                '            LocalVoltageReadresult = iec.AsyncIEC61850.result
-                '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
-
-                '            Debug.WriteLine("------------------- Reading Local & Source Voltage Done (61850) -------------------")
-
-                '        Next
-                '    End If
-
-            Catch ex As Exception
-                Interlocked.Increment(errorCounter)
-                Dim message As String = $"{Now}{vbCrLf}{ex.StackTrace}:{vbCrLf}{ex.Message}"
-                SetText(rvbForm.lblMsgCenter, message)
-                sb.AppendLine(message)
-            End Try
-        End Sub
     End Class
 End Namespace
--- rvbsim/Periodics/PeriodicEventsWrite.vb
+++ rvbsim/Periodics/PeriodicEventsWrite.vb
@@ -1,5 +1,4 @@
-﻿Imports System.Text
-Imports System.Threading
+﻿Imports System.Threading
 
 'custom libraries
 Imports tcpdnp.AsyncDNP3_0
@@ -74,45 +73,9 @@ Namespace PeriodicOperations
                                                      address:=registerBox.Value,
                                                      value:=Interlocked.Read(FwdRVBVoltages2Write(regulatorId)))
 
-                                                'write back calculated Forward RVB Voltage to specified dnp point
-                                                'dnp.Send(ManualEvent:=WriteEvent,
-                                                '         Destination:=rvbForm.DNPDestinationReg1.Value,
-                                                '         Source:=rvbForm.DNPSourceReg1.Value,
-                                                '         FunctionCode:=Mode.DirectOp,
-                                                '         ObjectX:=Objects.AnalogOutput,
-                                                '         Variation:=Variations.AnaOutBlockShort,
-                                                '         Qualifier:=QualifierField.AnaOutBlock16bitIndex,
-                                                '         Start16Bit:=1,
-                                                '         Stop16Bit:=registerBox.Value,                                      'fRVBVoltage.Value,
-                                                '         Value:=Interlocked.Read(FwdRVBVoltages2Write(regulatorId)),        'CUShort(Forward_RVBVoltage2Write),
-                                                '         Status:=0)
-                                                'WriteEvent.WaitOne()
-                                                'ReceivedErrorMsg = ErrorReceived
-
-                                                ''transmit Reverse RVB Voltage
-                                                'WriteEvent.Reset()
-
                                             ElseIf registerBox.Name.Contains("RRVB") Then
 
                                                 Debug.WriteLine($"Src:{Interlocked.Read(RevRVBVoltages2Write(regulatorId))}")
-
-                                                'write back calculated Reverse RVB Voltage to specified dnp point
-                                                'dnp.Send(ManualEvent:=WriteEvent,
-                                                '         Destination:=rvbForm.DNPDestinationReg1.Value,
-                                                '         Source:=rvbForm.DNPSourceReg1.Value,
-                                                '         FunctionCode:=Mode.DirectOp,
-                                                '         ObjectX:=Objects.AnalogOutput,
-                                                '         Variation:=Variations.AnaOutBlockShort,
-                                                '         Qualifier:=QualifierField.AnaOutBlock16bitIndex,
-                                                '         Start16Bit:=1,
-                                                '         Stop16Bit:=registerBox.Value,                                      'rRVBVoltage.Value,
-                                                '         Value:=Interlocked.Read(RevRVBVoltages2Write(regulatorId)),        'CUShort(Reverse_RVBVoltage2Write),
-                                                '         Status:=0)
-                                                'WriteEvent.WaitOne()
-                                                'ReceivedErrorMsg = ErrorReceived
-
-                                                ''transmit Reverse RVB Voltage
-                                                'WriteEvent.Reset()
 
                                                 Send(rvbForm:=rvbForm,
                                                      address:=registerBox.Value,
@@ -122,6 +85,9 @@ Namespace PeriodicOperations
 
 
                                     End Select
+
+
+                                    Dim s = New UpdateMeteringValues(rvbForm:=rvbForm, registerBox:=registerBox, regulatorId:=regulatorId)
 
                                     Debug.WriteLine($"------------------- Writing RVB Voltage ({ProtocolInUse}) Done -------------------")
 
@@ -175,116 +141,6 @@ Namespace PeriodicOperations
 
             'transmit Reverse RVB Voltage
             WriteEvent.Reset()
-
-        End Sub
-
-        <Obsolete("will remove in next iteration", True)>
-        Protected Friend Sub Write(ByRef rvbForm As RVBSim, regulatorId As Integer)
-
-            Try
-
-
-                ' TODO: delete up to exit sub
-                Debug.WriteLine($"{Date.Now:hh:mm:ss.ffff} -- {NameOf(Write)} is running... Regulator: {regulatorId + 1}")
-
-                If regulatorId < 0 Or regulatorId > 2 Or IsNothing(regulatorId) Then
-                    Debugger.Break()
-                End If
-
-                Debug.WriteLine($"<----------------------- Regulator: {regulatorId + 1}, Writing Elapsed time: {WritingTimers(regulatorId).ElapsedMilliseconds} msec, Reading Elapsed time: {ReadingTimer.ElapsedMilliseconds} msec")
-
-                WriteRegisterWaits.Item(regulatorId).Unregister(Nothing)
-                periodicReset.Timers(rvbForm:=rvbForm, regulatorID:=regulatorId)
-
-                Exit Sub
-
-                Dim WriteEvent As New ManualResetEvent(False)
-
-                Dim query = From regulator In testJsonSettingsRegulators.Regulator
-                            Where regulator.Id = regulatorId
-                            Select regulator.Values
-
-                For Each value In query
-
-                    For Each somethinElse In value
-
-
-                        If somethinElse.Name.Contains("RVBValue") Then
-
-                            Dim settingControlName As String = $"{ProtocolInUse}{somethinElse.Name}Reg{regulatorId}"
-                            Debug.WriteLine($" <------------------- {settingControlName} processing ...")
-
-                            Dim v() As Control = rvbForm.Controls.Find(settingControlName, True)
-
-                            If v.Length > 0 Then
-
-                                If v(0).Visible Then
-
-                                    Debug.Write($"--- {v(0).Name} is VISIBLE --- ")
-
-                                    Dim registerBox As NumericUpDown = CType(v(0), NumericUpDown)
-
-
-                                    Debug.WriteLine($"{Date.Now():mm:ss.ffff} - What????{ProtocolInUse}{somethinElse.Name}Reg{regulatorId}")
-
-                                    Debug.WriteLine("------------------- Writing RVB Voltage (MODBUS) -------------------")
-
-                                    GenerateRVBVoltage2Transfer(rvbForm:=rvbForm, regulatorNumber:=regulatorId)
-
-
-                                    'write back calculated Forward RVB Voltage to specified modbus register
-                                    ' Dim fRVBVoltage = rvbForm.ModbusSettingsGroup.GetChildControls(Of NumericUpDown)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.FRVBValue)}Reg1"))(0)       '{model.Id}"))
-
-                                    modbusWrite.WriteSingleRegister(registerBox.Value, CUShort(Forward_RVBVoltage2Write))
-
-                                    'write back calculated Reverse RVB Voltage to specified modbus register
-                                    ' Dim rRVBVoltage = rvbForm.ModbusSettingsGroup.GetChildControls(Of NumericUpDown)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.RRVBValue)}Reg1"))(0)     '{model.Id}"))
-
-                                    modbusWrite.WriteSingleRegister(registerBox.Value, CUShort(Reverse_RVBVoltage2Write))
-
-                                    ' outputFwdString.Append($"Reg{model.Id}: Fwd: {FormatNumber(Forward_RVBVoltage2Write / BecoCommunicationScaleFactor, 1)}{vbTab} -- {vbTab}")
-                                    ' outputRevString.Append($"Reg{model.Id}: Rev: {FormatNumber(Reverse_RVBVoltage2Write / BecoCommunicationScaleFactor, 1)}{vbTab} -- {vbTab}")
-                                    Debug.WriteLine("------------------- Writing RVB Voltage (MODBUS) Done -------------------")
-
-                                    WriteRegisterWaits.Item(regulatorId).Unregister(Nothing)
-                                    periodicReset.Timers(rvbForm:=rvbForm, regulatorID:=regulatorId)
-
-                                End If
-                            End If
-
-                        End If
-                    Next
-
-
-
-                    '    Debug.Write($"value: {value.Name}")
-
-                    '    ' it is not a metering control
-                    '    If value.Name.Contains("RVB") Then
-                    '        Debug.WriteLine($" <------------------- SKIPPED ...")
-                    '        Continue For
-                    '    End If
-
-                    '    Dim settingControlName As String = $"{ProtocolInUse}{value.Name}Reg{regulator.Id}"
-                    '    Debug.WriteLine($" <------------------- {settingControlName} processing ...")
-
-                    '    Dim v() As Control = rvbForm.Controls.Find(settingControlName, True)
-
-                    '    If v.Length > 0 Then
-
-                    '        If v(0).Visible Then
-
-                Next
-
-
-            Catch ex As Exception
-
-                Interlocked.Increment(errorCounter)
-                CheckErrors()
-                Dim message As String = $"{Now}{vbCrLf}{ex.StackTrace}:{vbCrLf}{ex.Message}"
-                SetText(rvbForm.lblMsgCenter, message)
-                sb.AppendLine(message)
-            End Try
 
         End Sub
     End Class
