1b83544a1776eeda4ae3a46dc567d47c13a8e031
tbircek <tbircek@beckwithelectric.com>
2020-08-05 10:35:31 -0400

    code clean up

--- rvbsim/Periodics/PeriodicEventsRead.vb
+++ rvbsim/Periodics/PeriodicEventsRead.vb
@@ -13,17 +13,16 @@ Namespace PeriodicOperations
 
                 For Each regulator In testJsonSettingsRegulators.Regulator
 
-                    Debug.WriteLine($"3-ph Active: {rvbForm.RVBSettings3Phase.Visible}")
-
                     ' only send to regulator 1 when Single Phase checked
                     If rvbForm.RVBSettings3Phase.Visible Or regulator.Id = 1 Then
+
+                        Debug.WriteLine($"3-ph Active: {rvbForm.RVBSettings3Phase.Visible}")
 
                         ' generate control names with regulator values.
                         For Each value In regulator.Values
 
                             ' it is not a metering control
                             If value.Name.Contains("RVB") Then
-                                ' Debug.WriteLine($" <------------------- SKIPPED ...")
                                 Continue For
                             End If
 
@@ -58,8 +57,8 @@ Namespace PeriodicOperations
                                                      Destination:=rvbForm.DNPDestinationReg1.Value,
                                                      Source:=rvbForm.DNPSourceReg1.Value,
                                                      FunctionCode:=Mode.Read,
-                                                     ObjectX:=Objects.AnalogInput,  ' .AnalogOutputStatus, '
-                                                     Variation:=Variations.AnaInput16bitVar4,   '.AnaOutBlockShort, '
+                                                     ObjectX:=Objects.AnalogInput,
+                                                     Variation:=Variations.AnaInput16bitVar4,
                                                      Qualifier:=QualifierField.AnaInput16bitStartStop,
                                                      Start16Bit:=registerBox.Value,
                                                      Stop16Bit:=registerBox.Value)
@@ -79,6 +78,32 @@ Namespace PeriodicOperations
 
                                         Case "iec"
 
+                                            ' TODO: wait until M-6200B supports this protocol.
+
+                                            '        For Each model As IECCommunicationModel In regulator.IECCommunication
+
+                                            '            Debug.WriteLine("------------------- Reading Local &  Source Voltage (61850) -------------------")
+
+                                            '            'read the user specified 61850 objects.
+                                            '            Dim localVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.LocalVoltage)}Reg1"))(0)    '{model.Id}"))
+
+                                            '            iec61850.Send(ReadEvent, localVoltage.Text, "Read")
+                                            '            ReadEvent.WaitOne()
+                                            '            LocalVoltageReadresult = iec.AsyncIEC61850.result
+                                            '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
+
+                                            '            'read the user specified 61850 objects.
+                                            '            Dim sourceVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.SourceVoltage)}Reg1"))(0)     '{model.Id}"))
+
+                                            '            iec61850.Send(ReadEvent, sourceVoltage.Text, "Read")
+                                            '            ReadEvent.WaitOne()
+                                            '            LocalVoltageReadresult = iec.AsyncIEC61850.result
+                                            '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
+
+                                            '            Debug.WriteLine("------------------- Reading Local & Source Voltage Done (61850) -------------------")
+
+                                            '        Next
+                                            '    End If
 
                                         Case Else
 
@@ -86,7 +111,6 @@ Namespace PeriodicOperations
 
                                     Dim s = New UpdateMeteringValues(rvbForm:=rvbForm, registerBox:=registerBox, regulatorId:=regulator.Id - 1)
 
-                                    ' SetText(rvbForm.lblMsgCenter, $"Error: {ReceivedErrorMsg}")
                                     SetTextBox(rvbForm.ErrorsTextBox, $"Error: {ReceivedErrorMsg}")
                                 End If
                             End If
@@ -102,40 +126,12 @@ Namespace PeriodicOperations
 
                 ReadEvent.SafeWaitHandle.Close()
 
-                '    ElseIf ProtocolInUse() = "iec" Then
-
-                '        For Each model As IECCommunicationModel In regulator.IECCommunication
-
-                '            Debug.WriteLine("------------------- Reading Local &  Source Voltage (61850) -------------------")
-
-                '            'read the user specified 61850 objects.
-                '            Dim localVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.LocalVoltage)}Reg1"))(0)    '{model.Id}"))
-
-                '            iec61850.Send(ReadEvent, localVoltage.Text, "Read")
-                '            ReadEvent.WaitOne()
-                '            LocalVoltageReadresult = iec.AsyncIEC61850.result
-                '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
-
-                '            'read the user specified 61850 objects.
-                '            Dim sourceVoltage = rvbForm.IecSettingsGroup.GetChildControls(Of TextBox)().Where(Function(tb) tb.Name.Equals($"{model.Name}{NameOf(model.SourceVoltage)}Reg1"))(0)     '{model.Id}"))
-
-                '            iec61850.Send(ReadEvent, sourceVoltage.Text, "Read")
-                '            ReadEvent.WaitOne()
-                '            LocalVoltageReadresult = iec.AsyncIEC61850.result
-                '            ReceivedErrorMsg = iec.AsyncIEC61850.ErrorReceived
-
-                '            Debug.WriteLine("------------------- Reading Local & Source Voltage Done (61850) -------------------")
-
-                '        Next
-                '    End If
-
             Catch ex As Exception
                 Interlocked.Increment(errorCounter)
-                ResetMeteringLabels()
                 Dim message As String = $"{Now}{vbCrLf}{ex.Message}:{vbCrLf}{ex.StackTrace}"
-                ' SetText(rvbForm.lblMsgCenter, message)
-                SetTextBox(textbox:=rvbForm.ErrorsTextBox, text:=message)
+                SetTextBox(textbox:=rvbForm.ErrorsTextBox, text:=message, append:=True)
                 sb.AppendLine(message)
+                ResetMeteringLabels()
             End Try
         End Sub
 
