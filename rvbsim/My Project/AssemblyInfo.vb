9b7cd5c097729a4ca9f19038c243dbd3010440ea
tbircek <tbircek@beckwithelectric.com>
2020-10-28 08:51:12 -0400

    update the program to 5.0.0

--- rvbsim/My Project/AssemblyInfo.vb
+++ rvbsim/My Project/AssemblyInfo.vb
@@ -32,6 +32,6 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.4.0")>
-<Assembly: AssemblyFileVersion("4.0.0.0")>
+<Assembly: AssemblyVersion("5.0.1.0")>
+<Assembly: AssemblyFileVersion("5.0.1.0")>
 <Assembly: NeutralResourcesLanguage("en-US")>
